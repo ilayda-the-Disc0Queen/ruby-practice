7ce0ea388da6262d495d982c94acb99c7669e85d
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-08-18 18:16:19 +0100

    added comment

--- product_of_three.rb
+++ product_of_three.rb
@@ -5,4 +5,4 @@ def highest_product_of_three(array_of_ints)
 end
 
 p highest_product_of_three([1, 2, 3, 4])
-p highest_product_of_three( [-10, -10, 1, 3, 2])
+p highest_product_of_three( [-10, -10, 1, 3, 2]) # should return 300 X
