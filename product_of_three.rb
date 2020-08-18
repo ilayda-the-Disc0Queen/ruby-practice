f8396578cc614460108f7b1d0f9cf92657149385
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-08-18 18:15:42 +0100

    first attempt at solving product of 3 problem, but doesn't satisfy edge cases

---
+++ product_of_three.rb
@@ -0,0 +1,8 @@
+def highest_product_of_three(array_of_ints)
+  top_three_ints = array_of_ints.max(3)
+  top_three_ints
+  product = top_three_ints.reduce(:*)
+end
+
+p highest_product_of_three([1, 2, 3, 4])
+p highest_product_of_three( [-10, -10, 1, 3, 2])
