b08430c6af91c9fda70b39a5f6eee04c991d269f
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-08-19 13:13:48 +0100

    added abs method to deal with negative ints

--- product_of_three.rb
+++ product_of_three.rb
@@ -1,8 +1,13 @@
 def highest_product_of_three(array_of_ints)
-  top_three_ints = array_of_ints.max(3)
+  absolute_array_of_ints = []
+  array_of_ints.each do |number|
+    absolute_array_of_ints << number.abs
+  end
+  absolute_array_of_ints
+  top_three_ints = absolute_array_of_ints.max(3)
   top_three_ints
   product = top_three_ints.reduce(:*)
 end
 
-p highest_product_of_three([1, 2, 3, 4])
-p highest_product_of_three( [-10, -10, 1, 3, 2]) # should return 300 X
+highest_product_of_three([1, 2, 3, 4])
+highest_product_of_three([-10, -10, 1, 3, 2]) # should return 300 X
