dceeec395895bce255ad8680404b53d89eb65cf3
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-08-26 12:59:38 +0100

    got help modifying run_length_encoding method and it now works correctly

--- weeder_question.rb
+++ weeder_question.rb
@@ -32,30 +32,44 @@ end
 
 do_2_sum_to_integer?([2, 3, 7], 8) # should return false
 
-def run_length_encoding(input_string)
-  input_string
-  raise ArgumentError "Too short" if input_string.empty? || input_string.nil?
+# def run_length_encoding(input_string)
+#   input_string
+#   raise ArgumentError "Too short" if input_string.empty? || input_string.nil?
 
+#   char_counter = 0
+#   final_result = ''
+#   input_string.each_char.with_index do |current_char, index = 1|
+#     if current_char != input_string[index - 1] && !current_char.nil?
+#       char_counter = 1
+#       final_result += char_counter.to_s + current_char
+#     else current_char == input_string[index - 1]
+#       incremented_char_counter = (char_counter += 1)
+#       p final_result = incremented_char_counter.to_s + current_char
+#     end
+
+#   end
+#   final_result
+# end
+
+# run_length_encoding("aaabbca") # should return "3a2b1c1a" but is returning "1a2a3a1b2b1c1a"
+
+def correct_run_length_encoding(input_string)
+  raise ArgumentError "Too short" if input_string.empty? || input_string.nil?
   char_counter = 0
+  index = 1
   final_result = ''
-  input_string.each_char.with_index do |current_char, index = 1|
-    if current_char != input_string[index - 1] && !current_char.nil?
-      char_counter = 1
-      final_result += char_counter.to_s + current_char
-    else current_char == input_string[index - 1]
-      # char_counter
-      incremented_char_counter = (char_counter += 1).to_s
-      final_result += incremented_char_counter + current_char
+  input_string.each_char do |current_char|
+    while current_char == input_string[index - 1] && !current_char.nil?
+      char_counter += 1
+      index += 1
     end
+    p final_result += char_counter.to_s + current_char if char_counter != 0
+    char_counter = 0
   end
   p final_result
 end
 
-run_length_encoding("aaabbca") # should return "3a2b1c1a"
-                     0123456
-
-
-
+correct_run_length_encoding("aaabbfffffcab")
 
 
 
